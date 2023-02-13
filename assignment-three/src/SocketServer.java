dcc5bd12c161f28f69d40168a9749667bbe381a7
Sander Johansen <sander.olin@gmail.com>
2023-02-13 20:56:46 +0100

    Implemented SocketThread to handle multiple clients.

--- assignment-three/src/SocketServer.java
+++ assignment-three/src/SocketServer.java
@@ -2,45 +2,19 @@ import java.io.*;
 import java.net.*;
 
 public class SocketServer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        final int PORT_NR = 1250;
-        ServerSocket server = new ServerSocket(PORT_NR);
-        System.out.println("Waiting for connection...");
-        Socket connection = server.accept();
-        System.out.println("Connection made with client");
-
-        InputStreamReader readingConnection = new InputStreamReader(connection.getInputStream());
-        BufferedReader reader = new BufferedReader(readingConnection);
-        PrintWriter writer = new PrintWriter(connection.getOutputStream(), true);
-
-        writer.println("Connection has been made between client and server.");
-        writer.println("Input complete math expression ((only adding and subtracting)): ");
-        try {
-            String line = reader.readLine().replaceAll("\\s", "");
-            while (line != null) {
-                System.out.println("Client wrote: " + line);
-                writer.println(line + " = " + calculateExpression(line));
-                writer.println("Input complete math expression (only adding and subtracting): ");
-                line = reader.readLine().replaceAll("\\s", "");
+        final int PORT_NR = 1337;
+        int nrOfConnections = 0;
+        try (ServerSocket serverSocket = new ServerSocket(PORT_NR)){
+            System.out.println("Server is online");
+            while (true) {
+                new SocketThread(serverSocket.accept()).start();
+                nrOfConnections++;
+                System.out.println("New connection: " + nrOfConnections);
             }
         } catch (IOException exception){
-            writer.println(exception.getMessage());
+            System.out.println(exception.getMessage());
         }
-        writer.close();
-        reader.close();
-        connection.close();
-    }
-
-    public static int calculateExpression(String line) throws IOException{
-        String[] split = line.split("(?<=\\d)(?=\\D)|(?<=\\D)(?=\\d)");
-        int result = Integer.parseInt(split[0]);
-        for (int i = 1; i < split.length; i+=2){
-            switch (split[i]) {
-                case "+" -> result += Integer.parseInt(split[i + 1]);
-                case "-" -> result -= Integer.parseInt(split[i + 1]);
-            }
-        }
-        return result;
     }
 }
---
+++ assignment-three/src/SocketThread.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SocketThread extends Thread{
+    private final Socket socket;
+
+    public SocketThread(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStreamReader readingConnection = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(readingConnection);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.println("Connection has been made between client and server.");
+            writer.println("Type empty string and enter to quit.");
+            writer.println("Input complete math expression (only adding and subtracting): ");
+            try {
+                String line = reader.readLine();
+                while (line != null && !line.isEmpty()) {
+                    line = line.replaceAll("\\s", "");
+                    System.out.println("Client wrote: " + line);
+                    int result;
+                    try {
+                        result = calculateExpression(line);
+                        writer.println(line + " = " + result);
+                    } catch (NumberFormatException exception){
+                        writer.println("Math expression error, try again.");
+                    }
+                    writer.println("Input complete math expression (only adding and subtracting): ");
+                    line = reader.readLine();
+                }
+            } catch (IOException exception){
+                writer.println(exception.getMessage());
+            }
+            writer.close();
+            reader.close();
+            socket.close();
+        } catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public static int calculateExpression(String line) throws IOException, NumberFormatException{
+        String[] split = line.split("(?<=\\d)(?=\\D)|(?<=\\D)(?=\\d)");
+        int result = Integer.parseInt(split[0]);
+        for (int i = 1; i < split.length; i+=2){
+            switch (split[i]) {
+                case "+" -> result += Integer.parseInt(split[i + 1]);
+                case "-" -> result -= Integer.parseInt(split[i + 1]);
+            }
+        }
+        return result;
+    }
+
+}
