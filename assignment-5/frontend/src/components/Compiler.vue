4eb501021cea356cf38aaf316ee1326537c3c19b
Sander Johansen <sander.olin@gmail.com>
2023-03-03 00:11:00 +0100

    Started Implementing frontend of web compiler

--- assignment-5/frontend/src/components/Compiler.vue
+++ assignment-5/frontend/src/components/Compiler.vue
@@ -1,76 +1,100 @@
 <template>
-  
+  <div id="grid-container">
+  <div class="grid-item">
+    <div>Selected: {{ selected }}</div>
+
+    <select id="select" v-model="selected">
+      <option>C++</option>
+      <option>Java</option>
+    </select>
+
+    <textarea 
+      class="coding" 
+      id="code-input" 
+      v-model="code" 
+      :disabled="selected == ''" 
+      placeholder="Write code">
+    </textarea>
+
+    <button 
+      id="compile-button" 
+      @click="runCompiler">
+      Compile and Run
+    </button>
+  </div>
+
+  <div class="spacing"></div>
+
+  <div class="grid-item">
+    <textarea 
+      class="coding" 
+      id="code-output" 
+      v-model="output" 
+      disabled>{{ output }}
+    </textarea>
+  </div>
+</div>
 </template>
 
+<script lang="ts">
+
+  export default{
+    name: "Compiler",
+    
+    data() {
+      return {
+          selected: "",
+          code: "",
+          output: "Code"
+      }
+    },
+    methods: {
+      runCompiler(){
+      }
+    }
+  }
+</script>
+
+
 <style scoped>
-.item {
-  margin-top: 2rem;
+
+#grid-container{
   display: flex;
 }
 
-.details {
-  flex: 1;
-  margin-left: 1rem;
+.grid-item{
+  display: grid;
+}
+.spacing{
+  min-width: 50px;
+}
+#select{
+  max-width: 400px;
 }
 
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-
-  color: var(--color-text);
+.coding{
+  background-color: #2c3e50;
+  color: white;
+  border: none;
+  resize: none;
+  height: 350px;
+  width: 400px;
+  padding: 10px;
 }
 
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
+#code-output{
+  margin-top: 46px;
 }
 
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
+#compile-button{
+  border-radius: 5px;
+  display: block;
+  width: 100px;
+  height: 40px;
+  margin: 10px auto 0px auto;
+  color: white;
+  background-color: green;
+  border: black;
+  cursor: pointer;
 }
 </style>
