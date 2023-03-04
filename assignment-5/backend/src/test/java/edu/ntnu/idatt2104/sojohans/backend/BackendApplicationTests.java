06f7630dc7ddbf78963a9cb2bdb408479da97edd
Sander Johansen <sander.olin@gmail.com>
2023-03-04 14:21:34 +0100

    Added Spring Boot backend to project

---
+++ assignment-5/backend/src/test/java/edu/ntnu/idatt2104/sojohans/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2104.sojohans.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
