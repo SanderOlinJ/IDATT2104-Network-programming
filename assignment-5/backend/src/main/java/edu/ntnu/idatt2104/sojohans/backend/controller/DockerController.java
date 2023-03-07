28da9195eb3719039db5c588361d1209d4e9ff3f
Sander Johansen <sander.olin@gmail.com>
2023-03-07 11:39:48 +0100

    Implemented Logger to backend, fixed some CSS

--- assignment-5/backend/src/main/java/edu/ntnu/idatt2104/sojohans/backend/controller/DockerController.java
+++ assignment-5/backend/src/main/java/edu/ntnu/idatt2104/sojohans/backend/controller/DockerController.java
@@ -2,6 +2,9 @@ package edu.ntnu.idatt2104.sojohans.backend.controller;
 
 import edu.ntnu.idatt2104.sojohans.backend.model.Code;
 import edu.ntnu.idatt2104.sojohans.backend.service.DockerCompilerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +15,18 @@ public class DockerController {
     @Autowired
     DockerCompilerService dockerCompilerService;
 
+    private Logger logger = LoggerFactory.getLogger(DockerController.class);
+
     @PostMapping("/compile")
     @ResponseBody
     public Code postCode(@RequestBody Code code){
-        return this.dockerCompilerService.compileAndRunCode(code);
+        logger.info("Code received: " + code.getCode());
+        this.dockerCompilerService.compileAndRunCode(code);
+        if (!code.getError().equals("")){
+            logger.info("Code error: " + code.getError());
+        } else {
+            logger.info("Code output: " + code.getCompiled());
+        }
+        return code;
     }
 }
